5b4df2b158ba92f776e554f6bc60be3a127c3db2
anishapareek <pareekanisha26@gmail.com>
2024-06-27 17:04:16 +0530

    Initial Commit

---
+++ ChatWithMe/ChatWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithMeApp.swift
+//  ChatWithMe
+//
+//  Created by Anisha Pareek on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
